6b13aadd312f726244a0a8d8bc240d41688dab59
Hacker Gaming <69993535+Sandi950@users.noreply.github.com>
2020-10-23 19:59:33 +0700

    Add files via upload

---
+++ Pw.sh
@@ -0,0 +1,42 @@
+bi='\033[34;1m' #biru
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cya
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+sleep 1
+echo $i"[Mencoba] Rendi12"
+sleep 1
+echo $i"[Mencoba] Man Jada Wa Jada"
+sleep 1
+echo $i"[Mencoba] Persija1928"
+sleep 1
+echo $i"[Mencoba] Sayangku25"
+sleep 1
+echo $i"[Mencoba] Andri56"
+sleep 1
+echo $i"[Mencoba] Renata77"
+sleep 1
+echo $i"[Mencoba] Afgan12"
+sleep 1
+echo $i"[Mencoba] Reno22"
+sleep 1
+echo $i"[Mencoba] Aning56"
+sleep 1
+echo $i"[Mencoba] Lulu"
+sleep 1
+echo $i"[Mencoba] Hendra"
+sleep 1
+echo $i"[Mencoba] Arya Suntoso77"
+sleep 1
+echo $i"[Mencoba] Susilo Bambang66"
+sleep 1
+echo $i"[Mencoba] Indonesia Hebat"
+sleep 1
+echo $i"[Mencoba] Ayam Goreng"
+sleep 1
+echo $i"[Mencoba] Kepo Lu77"
+sleep 1
+echo $i"[Mencoba] Upin Ipin Gak Gede Gede"
+
---
+++ pw.sh
@@ -0,0 +1,33 @@
+bi='\033[34;1m' #biru
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cya
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+sleep 1
+echo $i"[Mencoba] Garuda17"
+sleep 1
+echo $i"[Mencoba] Anak Pintar"
+sleep 1
+echo $i"[Mencoba] Sangkuriang"
+sleep 1
+echo $i"[Mencoba] fanns Alok"
+sleep 1
+echo $i"[Mencoba] Kepala Budi"
+sleep 1
+echo $i"[Mencoba] Game 8bit"
+sleep 1
+echo $i"[Mencoba] Raden71"
+sleep 1
+echo $i"[Mencoba] Cika16"
+sleep 1
+echo $i"[Mencoba] eyang89"
+sleep 1
+echo $i"[Mencoba] 8Bit Gak Ada Pintu"
+sleep 1
+echo $i"[Mencoba] Garena71"
+sleep 1
+echo $i"[Mencoba] Aku Suka Sama Kamu"
+sleep 1
+echo $i"[Mencoba] Moonton Garena"
